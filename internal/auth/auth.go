18998437960492b6ecfda9d905dda67946544b81
swhoosh <wish.kudomlap@gmail.com>
2025-09-25 14:55:03 +0700

    fix: fix bug

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,9 +12,9 @@ var ErrMalformedAuthHeader = errors.New("malformed authorization header")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
-	// if authHeader == "" {
-	// 	return "", ErrNoAuthHeaderIncluded
-	// }
+	if authHeader == "" {
+		return "", ErrNoAuthHeaderIncluded
+	}
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", ErrMalformedAuthHeader
